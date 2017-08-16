3582133c7ad922c51ca5ee44a772a4468288ab90
Dan Homutescu <dan.homutescu@orange.com>
2017-08-16 17:46:21 +0300

    added external config

--- spring/sportsapp/src/main/java/sportsapp/ro/SportsappApplication.java
+++ spring/sportsapp/src/main/java/sportsapp/ro/SportsappApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "sportsapp.ro")
+@PropertySource(value = "file:./config/libertas.properties")
 public class SportsappApplication {
 
 	public static void main(String[] args) {
